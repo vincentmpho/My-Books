9a4abb4f42506460e8ee8b465f1c80670ead1e58
Tshindimpho <mudzu13@gmail.com>
2024-04-22 11:34:43 +0200

    Create Seeding data for testing

--- My-Books/Data/AppDbContext.cs
+++ My-Books/Data/AppDbContext.cs
@@ -7,9 +7,11 @@ namespace My_Books.Data
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
         {
-            
+            DbInitializer.Initialize(this);
         }
 
         public DbSet<Book> Books { get; set; }
+
+
     }
 }
---
+++ My-Books/Data/DbInitializer.cs
@@ -0,0 +1,49 @@
+﻿using My_Books.Models;
+
+namespace My_Books.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AppDbContext context)
+        {
+            if (context.Books.Any())
+            {
+                return; // Data is already seeded
+            }
+
+            var books = new Book[]
+            {
+                new Book {
+                    Title = "Book 1",
+                    Description = "Description 1", 
+                    IsRead = true,
+                    DateRead = DateTime.Now, 
+                    Rate = 4,
+                    Genre = "Fiction",
+                    Author = "Author 1",
+                    CoverUrl = "url1",
+                    DateAdded = DateTime.Now 
+                },
+                new Book { 
+                    Title = "Book 2",
+                    Description = "Description 2", 
+                    IsRead = false,
+                    DateRead = null, 
+                    Rate = null,
+                    Genre = "Non-Fiction",
+                    Author = "Author 2",
+                    CoverUrl = "url2",
+                    DateAdded = DateTime.Now 
+                }
+               
+            };
+
+            foreach (var b in books)
+            {
+                context.Books.Add(b);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
