66a74213471c0c7ef74a6873c88ee2d4f2241b03
Tshindimpho <mudzu13@gmail.com>
2024-04-22 15:20:42 +0200

    Implement Repository pattern

---
+++ My-Books/Repository/BooksRepository.cs
@@ -0,0 +1,77 @@
+﻿using My_Books.Data;
+using My_Books.Models;
+using My_Books.Models.DTOs;
+using My_Books.Repository.Interface;
+using System.Collections.Generic;
+
+namespace My_Books.Repository
+{
+    public class BooksRepository : IBooksRepository
+    {
+        private readonly AppDbContext _context;
+
+        public BooksRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBook(BookDto book)
+        {
+            var _book = new Book()
+            {
+                Title = book.Title,
+                Description = book.Description,
+                IsRead = book.IsRead,
+                DateRead = book.IsRead ? book.DateRead.Value : null,
+                Rate = book.IsRead ? book.Rate.Value : null,
+                Genre = book.Genre,
+                Author = book.Author,
+                CoverUrl = book.CoverUrl,
+                DateAdded = DateTime.Now,
+            };
+
+            _context.Books.Add(_book);
+            _context.SaveChanges();
+        }
+
+        public void DeleteBook(int bookId)
+        {
+            var book = _context.Books.Find(bookId);
+            if (book != null)
+            {
+                _context.Books.Remove(book);
+                _context.SaveChanges();
+            }
+        }
+
+        public List<Book> GetAllBooks()
+        {
+            var allBooks = _context.Books.ToList();
+            return allBooks;
+        }
+
+        public Book GetBookById(int bookId)
+        {
+            var book = _context.Books.FirstOrDefault(x=>x.Id==bookId);
+            return book;
+        }
+
+        public void UpdateBook(int bookId, BookDto book)
+        {
+            var _book = _context.Books.Find(bookId);
+
+            if (_book != null)
+            {
+                _book.Title = book.Title;
+                _book.Description = book.Description;
+                _book.IsRead = book.IsRead;
+                _book.DateRead = book.IsRead ? book.DateRead.Value : null;
+                _book.Rate = book.IsRead ? book.Rate.Value : null;
+                _book.Genre = book.Genre;
+                _book.Author = book.Author;
+                _book.CoverUrl = book.CoverUrl;
+                _context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ My-Books/Repository/Interface/IBooksRepository.cs
@@ -0,0 +1,16 @@
+﻿using My_Books.Models;
+using My_Books.Models.DTOs;
+
+namespace My_Books.Repository.Interface
+{
+    public interface IBooksRepository 
+    {
+        void AddBook(BookDto book);
+
+        List<Book> GetAllBooks();
+        Book GetBookById(int bookId);
+        void UpdateBook(int bookId, BookDto book);
+        void DeleteBook(int bookId);
+
+    }
+}
