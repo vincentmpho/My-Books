2026a6b2f7a9e6964a8daefcc3c57eed12703d8f
Tshindimpho <mudzu13@gmail.com>
2024-04-22 11:16:35 +0200

    Add project files.

---
+++ My-Books/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using My_Books.Models;
+
+namespace My_Books.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
