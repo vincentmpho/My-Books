5a22f16392954c25af3329332d6831b23825dcbd
Tshindimpho <mudzu13@gmail.com>
2024-04-22 15:20:02 +0200

    Add DTO

---
+++ My-Books/Models/DTOs/BookDto.cs
@@ -0,0 +1,14 @@
+﻿namespace My_Books.Models.DTOs
+{
+    public class BookDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool IsRead { get; set; }
+        public DateTime? DateRead { get; set; }
+        public int? Rate { get; set; }
+        public string Genre { get; set; }
+        public string Author { get; set; }
+        public string CoverUrl { get; set; }
+    }
+}
